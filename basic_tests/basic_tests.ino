1170288798f84cc4328ec700489fae9efaf4db61
guutz <findmagpie@gmail.com>
2022-11-30 22:05:03 -0800

    added GPS parsing

--- basic_tests/basic_tests.ino
+++ basic_tests/basic_tests.ino
@@ -1,10 +1,15 @@
 
 #include <RadioLib.h>
+#include <Adafruit_GPS.h>
+
+// Connect to the GPS on the hardware I2C port
+Adafruit_GPS GPS(&Wire);
 
 // RF69 has the following connections:
 // CS pin:    9
 // DIO0 pin:  2
 // RESET pin: 3
+
 RF69 radio = new Module(9, 2, 3);
 
 
@@ -13,7 +18,14 @@ int transmissionState = RADIOLIB_ERR_NONE;
 void setup() {
   Serial.begin(9600);
 
+  GPS.begin(0x10); //I2C address
   // initialize RF69 with default settings
+
+  GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
+
+  GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ); // 1 Hz update rate
+
+  GPS.sendCommand(PGCMD_ANTENNA);
   Serial.print(F("[RF69] Initializing ... "));
   int state = radio.begin();
   if (state == RADIOLIB_ERR_NONE) {
@@ -71,14 +83,18 @@ void setup() {
 
   Serial.print(F("[RF69] Sending first packet ... "));
 
-//  transmissionState = radio.startTransmit("Hello World!");
+  transmissionState = radio.startTransmit("Hello World!");
+    //altitude
+    //gps
+    //acceleration
+    //
 
-    byte byteArr[] = {0x01, 0x23, 0x45, 0x67,
-                      0x89, 0xAB, 0xCD, 0xEF,0x01, 0x23, 0x45, 0x67,
-                      0x89, 0xAB, 0xCD, 0xEF,0x01, 0x23, 0x45, 0x67,
-                      0x89, 0xAB, 0xCD, 0xEF,0x01, 0x23, 0x45, 0x67,
-                      0x89, 0xAB, 0xCD, 0xEF};
-    transmissionState = radio.startTransmit(byteArr, 32);
+    // byte byteArr[] = {0x01, 0x23, 0x45, 0x67,
+    //                   0x89, 0xAB, 0xCD, 0xEF,0x01, 0x23, 0x45, 0x67,
+    //                   0x89, 0xAB, 0xCD, 0xEF,0x01, 0x23, 0x45, 0x67,
+    //                   0x89, 0xAB, 0xCD, 0xEF,0x01, 0x23, 0x45, 0x67,
+    //                   0x89, 0xAB, 0xCD, 0xEF};
+    // transmissionState = radio.startTransmit(byteArr, 32);
   
 }
 
@@ -95,7 +111,35 @@ void setFlag(void) {
 
 void loop() {
   
- 
+ String GPS_data = "";
+    char c = GPS.read();
+    if (GPS.newNMEAreceived()) {
+    // a tricky thing here is if we print the NMEA sentence, or data
+    // we end up not listening and catching other sentences!
+    // so be very wary if using OUTPUT_ALLDATA and trying to print out data
+    
+    Serial.println("nmea recieved");
+    String GPS_data = GPS.lastNMEA(); // this also sets the newNMEAreceived() flag to false
+    Serial.println(GPS_data);
+   
+    if (!GPS.parse(GPS.lastNMEA())) // this also sets the newNMEAreceived() flag to false
+      return; // we can fail to parse a sentence in which case we should just wait for another
+
+    
+    if (GPS.fix) {
+      Serial.print("Location: ");
+      Serial.print(GPS.latitude, 4); Serial.print(GPS.lat);
+      Serial.print(", ");
+      Serial.print(GPS.longitude, 4); Serial.println(GPS.lon);
+      Serial.print("Speed (knots): "); Serial.println(GPS.speed);
+      Serial.print("Angle: "); Serial.println(GPS.angle);
+      Serial.print("Altitude: "); Serial.println(GPS.altitude);
+      Serial.print("Satellites: "); Serial.println((int)GPS.satellites);
+    }
+    else {
+      Serial.print("No GPS fix ):");
+    }
+    
   if(transmittedFlag) {
     enableInterrupt = false;
     transmittedFlag = false;
@@ -107,22 +151,24 @@ void loop() {
     }
 
     
-    delay(3000);
 
+
+    }
+   
     
+
+
     Serial.print(F("[RF69] Sending another packet ... "));
-
-    //transmissionState = radio.startTransmit("Hello World!");
-
-
-    byte byteArr[] = {0x01, 0x23, 0x45, 0x67,
-                      0x89, 0xAB, 0xCD, 0xEF,0x01, 0x23, 0x45, 0x67,
-                      0x89, 0xAB, 0xCD, 0xEF,0x01, 0x23, 0x45, 0x67,
-                      0x89, 0xAB, 0xCD, 0xEF,0x01, 0x23, 0x45, 0x67,
-                      0x89, 0xAB, 0xCD, 0xEF};
-    transmissionState = radio.startTransmit(byteArr, 32);
+    transmissionState = radio.startTransmit("header" + GPS_data);
+    delay(1000);
+    //byte byteArr[] = {0x01, 0x23, 0x45, 0x67,
+      //                0x89, 0xAB, 0xCD, 0xEF,0x01, 0x23, 0x45, 0x67,
+        //              0x89, 0xAB, 0xCD, 0xEF,0x01, 0x23, 0x45, 0x67,
+          //            0x89, 0xAB, 0xCD, 0xEF,0x01, 0x23, 0x45, 0x67,
+            //          0x89, 0xAB, 0xCD, 0xEF};
+    //transmissionState = radio.startTransmit(byteArr, 32);
   
   
-    enableInterrupt = true;
+  
   }
 }
